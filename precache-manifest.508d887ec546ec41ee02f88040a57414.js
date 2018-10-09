22df186b364c261dd2b570d7d47a1431fbd1aaed
anjibabu <bandianjibabu8@gmail.com>
2018-10-09 14:43:24 +0530

    Updates

---
+++ precache-manifest.508d887ec546ec41ee02f88040a57414.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/joeyroom/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "3e6ab641dabcabdb7682",
+    "url": "/joeyroom/static/js/runtime~main.3e6ab641.js"
+  },
+  {
+    "revision": "3837bf4e753db172b71e",
+    "url": "/joeyroom/static/js/main.3837bf4e.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/joeyroom/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "3837bf4e753db172b71e",
+    "url": "/joeyroom/static/css/main.e41c7808.chunk.css"
+  },
+  {
+    "revision": "f0eb152d42f04cf099189cf615df9b8a",
+    "url": "/joeyroom/index.html"
+  }
+];
